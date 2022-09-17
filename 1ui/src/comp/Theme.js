7ff180c9bb95797bc239c9c1ab6ca666a60621c9
sr <sr@debian-BULLSEYE-live-builder-AMD64>
2022-09-17 14:34:31 +0530

    theme

--- 1ui/src/comp/Theme.js
+++ 1ui/src/comp/Theme.js
@@ -2,33 +2,36 @@ import React from "react";
 import logoDark from '../assets/theme-dark.gif';
 import logoLight from '../assets/theme-light.gif';
 
-export function SetTheme(themeName) {
-  localStorage.setItem('theme', themeName);
-  document.documentElement.className = themeName;
+export function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
 }
 
-export function GetTheme() {
-  return localStorage.getItem('theme');
+export function getTheme() {
+    return localStorage.getItem('theme');
 }
 
-export function ToggleTheme() {
-  if (GetTheme() === 'dark') {
-    SetTheme('light');
-  } else {
-    SetTheme('dark');
-  }
+export function toggleTheme() {
+    if (getTheme() === 'dark') {
+        setTheme('light');
+    } else {
+        setTheme('dark');
+    }
 }
 
 export default class Theme extends React.Component {
 
-  render() {
-    return (
-      <button className="theme-button" onClick={this.props.onClicked}>
-        { this.props.theme === 'dark' ?
-          <img className="theme-icon" title="ToggleTheme" alt="ToggleTheme" src={logoDark} /> :
-          <img className="theme-icon" title="ToggleTheme" alt="ToggleTheme" src={logoLight} />
-        }
-      </button>
-    );
-  }
+    render() {
+        return (
+            <button
+                className="theme-button"
+                onClick={this.props.onClicked}>
+
+                {this.props.theme === 'dark' ?
+                    <img className="theme-icon" title="ToggleTheme" alt="ToggleTheme" src={logoDark} /> :
+                    <img className="theme-icon" title="ToggleTheme" alt="ToggleTheme" src={logoLight} />
+                }
+            </button>
+        );
+    }
 }
--- 1ui/src/index.js
+++ 1ui/src/index.js
@@ -3,58 +3,51 @@ import { createRoot } from "react-dom/client";
 import "./style/index.css";
 import "purecss/build/base-min.css";
 import "purecss/build/grids-responsive-min.css";
-import {
-  Header,
-  Theme
-} from "./comp/CompList";
-import { Menus, CompMap } from './app/constants';
-import { GetTheme, ToggleTheme } from './comp/Theme';
+import { Header, Theme } from "./comp/CompList";
+import { Menus, CompMap } from "./app/constants";
+import { getTheme, toggleTheme } from "./comp/Theme";
 
 class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: "Home",
+            theme: getTheme(),
+        };
+    }
 
-  constructor(props) {
-    super(props);
-    this.headerClicked = this.headerClicked.bind(this);
-    this.toggleTheme = this.toggleTheme.bind(this);
-    this.state = {
-      show: "Home",
-      theme: GetTheme(),
+    headerClicked = (menu, e) => {
+        e.preventDefault();
+        this.setState({ show: menu });
     };
-  }
 
-  toggleTheme(){
-    ToggleTheme()
-    this.setState({ theme: GetTheme() });
-  }
+    render() {
+        const VisibleComponent = CompMap[this.state.show];
 
-  headerClicked(menu, e) {
-    e.preventDefault();
-    this.setState({ show: menu });
-  }
+        return (
+            <div>
+                <div className="pure-menu pure-menu-horizontal">
+                    {Menus.map((menu) => (
+                        <Header
+                            menu={menu}
+                            key={menu}
+                            onClicked={(e) => this.headerClicked(menu, e)}
+                        />
+                    ))}
 
-  render() {
-    const VisibleComponent = CompMap[this.state.show];
+                    <Theme
+                        theme={this.state.theme}
+                        onClicked={() => {
+                            toggleTheme();
+                            this.setState({ theme: getTheme() });
+                        }}
+                    />
+                </div>
 
-    return (
-      <div>
-        <div className="pure-menu pure-menu-horizontal">
-
-          {Menus.map((menu) => (
-            <Header
-              menu={menu}
-              key={menu}
-              onClicked={(e) => this.headerClicked(menu, e)}
-            />
-          ))}
-
-          <Theme theme={this.state.theme} onClicked={this.toggleTheme}/>
-
-        </div>
-
-        <VisibleComponent theme={this.state.theme}/>
-      </div>
-    );
-  }
+                <VisibleComponent theme={this.state.theme} />
+            </div>
+        );
+    }
 }
 
 const container = document.getElementById("root");
