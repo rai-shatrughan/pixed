48c7aad9f64cc80c42fb066c0405a15f9b86f32a
sr <sr@debian-BULLSEYE-live-builder-AMD64>
2022-10-18 14:38:55 +0530

    conf

--- microsvc/pkg/mware/config.go
+++ microsvc/pkg/mware/config.go
@@ -7,18 +7,18 @@ import (
 	"github.com/spf13/viper"
 )
 
-//Config is wrapper for config tasks
+// Config is wrapper for config tasks
 type Config struct {
 	*viper.Viper
 }
 
-//New intializes config defaults
+// New intializes config defaults
 func (c *Config) New() {
 	c.Viper = viper.New()
 	c.AddConfigPath("/etc/conf/") // path to look for the config file in
 	c.AddConfigPath("/conf")      // call multiple times to add many search paths
 	c.AddConfigPath("../conf/")
-	c.AddConfigPath("./mware/conf/")
+	c.AddConfigPath("./pkg/conf/")
 	c.SetConfigName("config")
 	c.SetConfigType("yaml")
 
