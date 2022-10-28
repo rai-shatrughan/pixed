e35e935bf98c294f76ec235e3b59682eca63763e
sr <sr@debian-BULLSEYE-live-builder-AMD64>
2022-10-28 16:52:39 +0530

    inp

--- rsvc/src/handler/exchange.rs
+++ rsvc/src/handler/exchange.rs
@@ -9,33 +9,33 @@ use rdkafka::producer::{FutureProducer, FutureRecord};
 use serde_json::json;
 use std::time::Duration;
 
-pub async fn post_ts(
-    Extension(kafka_producer): Extension<FutureProducer>,
-    Path(asset_id): Path<String>,
-    req: Request<Body>
-) -> impl IntoResponse {
-    // let payload_string = format!("{:?}", &payload);
+// pub async fn post_ts(
+//     Extension(kafka_producer): Extension<FutureProducer>,
+//     Path(asset_id): Path<String>,
+//     req: Request<Body>
+// ) -> impl IntoResponse {
+//     // let payload_string = format!("{:?}", &payload);
 
-    let body_bytes = match hyper::body::to_bytes(req.into_body()).await {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
+//     let body_bytes = match hyper::body::to_bytes(req.into_body()).await {
+//         Ok(v) => v,
+//         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+//     };
 
-    let payload_string = format!("{:?}", body_bytes);
+//     let payload_string = format!("{:?}", body_bytes);
 
-    kafka_producer
-    .send(
-        FutureRecord::to("ts")
-            .key(&asset_id)
-            .payload(&payload_string),
-        Duration::from_secs(0),
-    )
-    .await
-    .expect("Failed to put msg into Kafka");
+//     kafka_producer
+//     .send(
+//         FutureRecord::to("ts")
+//             .key(&asset_id)
+//             .payload(&payload_string),
+//         Duration::from_secs(0),
+//     )
+//     .await
+//     .expect("Failed to put msg into Kafka");
 
 
-    (StatusCode::OK, Json(json!({ "TS": "OK" })))
-}
+//     (StatusCode::OK, Json(json!({ "TS": "OK" })))
+// }
 
 pub async fn post_multi_ts(
     Extension(kafka_producer): Extension<FutureProducer>,
