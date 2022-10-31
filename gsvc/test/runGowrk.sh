93587b294bbbf46d9583b4bdcbd3e3c33aa8aabe
sr <sr@debian-BULLSEYE-live-builder-AMD64>
2022-10-31 17:58:03 +0530

    inp

--- gsvc/test/runGowrk.sh
+++ gsvc/test/runGowrk.sh
@@ -16,8 +16,8 @@ putTS(){
     go-wrk -c 8000 -d 3600 -T 30000 -M POST \
         -H "X-API-Key: sr12345" \
         -H "Content-Type: application/json" \
-        -body @test/json/ts.json \
-        http://${HOST}/api/v1/exchange/6fdae6af-226d-48bd-8b61-699758137eb3
+        -body @test/json/ex.json \
+        http://${HOST}/api/mindconnect/v3/exchange/6fdae6af-226d-48bd-8b61-699758137eb3
 
     end=`date`
     echo "End Time - " $end
@@ -33,7 +33,7 @@ getTS(){
 
         go-wrk -c 500 -d 30 -T 50000 -M GET \
         -H "X-API-Key: sr12345" \
-        http://172.18.0.21:8000/api/v1/exchange/6fdae6af-226d-48bd-8b61-699758137eb3?duration=1m
+        http://172.18.0.21:8000/api/iottimeseries/v3/timeseries/6fdae6af-226d-48bd-8b61-699758137eb3?duration=1m
 
     end=`date`
     echo "End Time - " $end
@@ -41,7 +41,7 @@ getTS(){
 
 getCurlTS(){
     curl -H "X-API-Key: sr12345" \
-    http://172.18.0.21:8000/api/v1/exchange/6fdae6af-226d-48bd-8b61-699758137eb3?duration=1m
+    http://172.18.0.21:8000/api/iottimeseries/v3/timeseries/6fdae6af-226d-48bd-8b61-699758137eb3?duration=1m
 }
 
 putTS
